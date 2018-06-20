458aeb48c289266b3d8511ca26e9d46382d21b2a
pranavhippargi <pranavhippargi@gmail.com>
2018-06-20 12:19:29 -0700

    Testing

--- send_sms.py
+++ send_sms.py
@@ -1,20 +1,15 @@
 from twilio.rest import Client
-from weatherdata import weather
+from weatherdata import Weather
 
-class main():
-   
-    def weatherString(self):
-        my_message = weather()
+class Main(Weather):
         
     def send_sms(self):
-        x = weather()
-        x.getWeather()
-        y = str(x)
+        x = Weather()
+        messageText = "It is " + str(x.getWeather()) + " Celcius, today"
         account_sid = 'AC1061e132398b5dc01334d9ff9b63d58a'
         auth_token = '9e6a04f6a51ef7d32858ac16f83c2727'
         client = Client(account_sid, auth_token)
         message = client.messages.create(to="+19163379294", from_="8459996189",
-                                        body=y)
-
-x = main()
+                                        body=messageText)
+x= Main()
 x.send_sms()
--- weatherdata.py
+++ weatherdata.py
@@ -4,30 +4,30 @@ from twilio.rest import Client
 
 
 # Get Weather Data using Open Weather API
-class weather():
+class Weather():
+    
+    def __init__(self):
+        pass
+        
     def getWeather(self):
         api_key = 'http://api.openweathermap.org/data/2.5/weather?appid=c38e770a1da2d9c102a1e4a5ae98f0d1&q='
         city = input("Input your city name: ")
         url = api_key + city
         data = requests.get(url).json()
-        formatted_data = data['main']['temp']
-        return formatted_data
+        weather_kelvin = data['main']['temp']
+        weather_celcius = weather_kelvin - 237.15
+        return ("%.2f" % weather_celcius)
         
-        
-        
-    def sendText(self):  
-        y = weather()
-        message = "It is " + str(y.getWeather()) + " today"
-        print(str(y) + " Kelvin")
-        account_sid = 'AC1061e132398b5dc01334d9ff9b63d58a'
-        auth_token = '9e6a04f6a51ef7d32858ac16f83c2727'
-        client = Client(account_sid, auth_token)
-        message = client.messages.create(to="+19163379294", from_="8459996189",
-                                        body=message)
+# class Text(Weather):   
 
-x = weather()
-# x.getWeather()
-x.sendText()
+#     def __init__(self):
+#         pass
+        
+#     def sendText(self): 
+#         y = Weather() 
+#         y.getWeather()
+#         message = "It is " + str(y.getWeather()) + " today"
+        
 
 
 
