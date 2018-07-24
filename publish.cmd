d0476aa8cd8afb0e25ac6df5bb0b9c8c99216abd
Pranav Hippargi <pranavhippargi>
2018-07-24 15:35:33 -0700

    integrate changes to botv4

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=kiyoung-test-Web-Deploy.pubxml -p:Password=YKluY4R6X7Ge6QWXYM4pcu3Add6aJwRZXDvn3ijYrrJFsrHrhS4R6X8bW0ql
-
