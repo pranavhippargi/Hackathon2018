e7e8e24e38cb72223c8633e96f13715a50e754a7
Pranav Hippargi <pranavhippargi@gmail.com>
2018-07-24 13:41:00 -0700

    Add cropinfo (#5)
    * add cropinfo
    
    * add message handler

---
+++ Crop/CropInfo.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+
+namespace HelpFarmerViaSMS
+{
+    public struct CropInfo
+    {
+        public string name;
+        public float lowPrice;
+        public float avgPrice;
+        public float highPrice;
+        public DateTime date;
+    }
+
+    public enum Crop
+    {
+        Corn = 0, Cocoa, Wheat, Oats, Rough Rice, Soybean, SoybeanMeal, SoybeanOil, Canola
+    }
+
+    public static class CropExtensions
+    {
+        public static string toString(this Crop crop)
+        {
+            switch (crop)
+            {
+                case Corn:
+                    return "corn";
+                case Cocoa:
+                    return "cocoa";
+                case Wheat:
+                    return "wheat";
+                case Oats:
+                    return "oats";
+                case Rough:
+                    return "rough";
+                case Soybean:
+                    return "soybean";
+                case SoybeanMeal: 
+                    return "soybeanMeal";
+                case SoybeanOil:
+                    return "soybeanOil";
+                case Canola:
+                    return "canola";
+            }
+        }
+    }
+}
