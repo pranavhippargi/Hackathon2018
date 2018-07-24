75b0a8e7092087733d0d171c9ef349f39f39c3bb
Young Kim <youngseongkim93@gmail.com>
2018-07-23 18:24:25 -0700

    make it work on azure bot

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: I'm just repeating what you are saying: {message.Text}");
+                await context.PostAsync($"{this.count++}: repeating: {message.Text}");
                 context.Wait(MessageReceivedAsync);
             }
         }
