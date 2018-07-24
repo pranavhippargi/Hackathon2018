9010e37e3910ee3eea35bf7b1586701fca99b7e4
Tanuja Kulal <takulal@microsoft.com>
2018-07-24 13:54:03 -0700

    Add project files.

---
+++ HelpingFarmerBot/EchoBot.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Schema;
+
+namespace HelpingFarmerBot
+{
+    public class EchoBot : IBot
+    {
+        /// <summary>
+        /// Every Conversation turn for our EchoBot will call this method. In here
+        /// the bot checks the Activty type to verify it's a message, bumps the 
+        /// turn conversation 'Turn' count, and then echoes the users typing
+        /// back to them. 
+        /// </summary>
+        /// <param name="context">Turn scoped context containing all the data needed
+        /// for processing this conversation turn. </param>        
+        public async Task OnTurn(ITurnContext context)
+        {
+            //// This bot is only handling Messages
+            //if (context.Activity.Type == ActivityTypes.Message)
+            //{
+            //    //// Get the conversation state from the turn context
+            //    //var state = context.GetConversationState<EchoState>();
+
+            //    //// Bump the turn count. 
+            //    //state.TurnCount++;
+
+            //    //// Echo back to the user whatever they typed.
+            //    //await context.SendActivity($"Turn {state.TurnCount}: TanuK sent '{context.Activity.Text}'");
+
+
+            //}
+
+
+            switch (context.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    var messagetext = context.Activity.Text.Trim().ToLower();
+
+                    // echo back the user's input.
+                    await context.SendActivity($"You sent '{context.Activity.Text}'");
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    foreach (var newMember in context.Activity.MembersAdded)
+                    {
+                        if (newMember.Id != context.Activity.Recipient.Id)
+                        {
+                            await context.SendActivity("Hello and welcome to the echo bot.");
+                            await context.SendActivity("Hello, you are choosing to use the Microsoft Translator API Bot.");
+                        }
+
+
+                        var activity = MessageFactory.SuggestedActions(
+                        new CardAction[]
+                        {
+                            new CardAction(title: "Crop Price", type: ActionTypes.ImBack, value: "Crop Price"),
+                            new CardAction( title: "green", type: ActionTypes.ImBack, value: "green"),
+                            new CardAction(title: "blue", type: ActionTypes.ImBack, value: "blue")
+                        }, text: "Choose a color");
+
+                        // Send the activity as a reply to the user.
+                        await context.SendActivity(activity);
+
+                    }
+                    break;
+            }
+
+        }
+    }
+}
